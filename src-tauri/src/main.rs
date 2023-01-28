4946a5f57f519eb455d61f9a81498cf8bf1cfc48
lly ke <2720851545@qq.com>
2023-01-28 10:29:20 +0800

    feat: 系统托盘

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -3,14 +3,30 @@
     windows_subsystem = "windows"
 )]
 
-mod setup;
+use env_logger::{Builder, Target};
+
 mod commands;
+mod setup;
+mod tray;
 
 fn main() {
+    log_init();
+
     let ctx = tauri::generate_context!();
     tauri::Builder::default()
-        .invoke_handler(tauri::generate_handler![commands::set_window_vibrancy_backend])
+        .invoke_handler(tauri::generate_handler![
+            commands::set_window_vibrancy_backend
+        ])
         .setup(setup::init)
+        .system_tray(tray::menu())
+        .on_system_tray_event(tray::handler)
         .run(ctx)
         .expect("error while running tauri application");
+}
+
+fn log_init() {
+    let mut builder = Builder::from_default_env();
+    builder.target(Target::Stdout);
+
+    builder.init();
 }
---
+++ src-tauri/src/tray.rs
@@ -0,0 +1,41 @@
+use log::{debug, info, warn};
+use tauri::{
+    AppHandle, CustomMenuItem, Manager, SystemTray, SystemTrayEvent, SystemTrayMenu,
+    SystemTrayMenuItem, SystemTraySubmenu,
+};
+
+pub fn menu() -> SystemTray {
+    let tray_menu = SystemTrayMenu::new()
+        // .add_native_item(SystemTrayMenuItem::Separator)
+        .add_item(CustomMenuItem::new("show".to_string(), "显示")) // 显示应用窗口
+        .add_item(CustomMenuItem::new("hide".to_string(), "隐藏")); // 隐藏应用窗口
+
+    SystemTray::new().with_menu(tray_menu)
+}
+
+pub fn handler(app: &AppHandle, event: SystemTrayEvent) {
+    match event {
+        SystemTrayEvent::MenuItemClick { tray_id, id, .. } => {
+            debug!("SystemTrayEvent::MenuItemClick: {}, {}", tray_id, id);
+
+            match id.as_str() {
+                // 显示隐藏窗口顺序会不一样, 没官方显示隐藏api先这样吧
+                "show" => {
+                    app.windows().iter().for_each(|(_, w)| {
+                        w.set_focus().unwrap();
+                        w.show().unwrap();
+                    });
+                    // app.show().unwrap();
+                }
+                "hide" => {
+                    app.windows().iter().for_each(|(_, w)| {
+                        w.hide().unwrap();
+                    });
+                    // app.hide().unwrap();
+                }
+                _ => {}
+            }
+        }
+        _ => {}
+    };
+}
