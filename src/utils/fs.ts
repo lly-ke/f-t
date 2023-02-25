db6326fd16e998717623425999bc8c6fa85c9535
lly ke <2720851545@qq.com>
2023-02-25 15:15:52 +0800

    feat: 配置文件兼容

--- src/utils/fs.ts
+++ src/utils/fs.ts
@@ -1,7 +1,9 @@
 import { readTextFile, writeTextFile, BaseDirectory, exists, createDir } from '@tauri-apps/api/fs'
 import { message } from 'ant-design-vue'
+import { getVersion } from '@tauri-apps/api/app'
 
 const thirdConfigPath = 'app.json'
+const version = await getVersion()
 
 export async function readThirdConfigWithYoudao() {
   const configList = await readThirdConfig()
@@ -20,6 +22,15 @@ export async function readThirdConfigWithXiaoniu() {
 }
 
 export async function readThirdConfig() {
+  const config = await readConfig()
+  return config?.config?.third || []
+}
+
+export async function writeThirdConfig(config: any) {
+  writeConfig({ version, config: { third: config } })
+}
+
+async function readConfig() {
   try {
     if (await exists(thirdConfigPath, { dir: BaseDirectory.AppConfig })) {
       const configJsonStr = await readTextFile(thirdConfigPath, { dir: BaseDirectory.AppConfig })
@@ -33,7 +44,7 @@ export async function readThirdConfig() {
   }
 }
 
-export async function writeThirdConfig(config: any) {
+async function writeConfig(config: any) {
   try {
     await writeTextFile(thirdConfigPath, JSON.stringify(config, null, 2), { dir: BaseDirectory.AppConfig })
     console.log('写入配置成功', config)
